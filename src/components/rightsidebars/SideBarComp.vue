784e739d19201c0e1194b9ab83c574599945dfb4
DEP <arkduni@gmail.com>
2023-03-25 15:40:39 +0900

    부트스트랩 임포트 관련 수정

--- src/components/rightsidebars/SideBarComp.vue
+++ src/components/rightsidebars/SideBarComp.vue
@@ -1,41 +1,118 @@
 <template>
     <div class="sticky-top">
-        <aside class="bd-aside sticky-xl-top text-muted align-self-start mb-3 mb-xl-5 px-2">
-            <h2 class="h6 pt-4 pb-3 mb-4 border-bottom">On this page</h2>
-            <nav class="small" id="toc">
-                <ul class="list-unstyled">
-                <li class="my-2">
-                    <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#contents-collapse" aria-controls="contents-collapse">Contents</button>
-                    <ul class="list-unstyled ps-3 collapse" id="contents-collapse">
-                    <li><a class="d-inline-flex align-items-center rounded" href="#typography">Typography</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#images">Images</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#tables">Tables</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#figures">Figures</a></li>
-                    </ul>
-                </li>
-                <li class="my-2">
-                    <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#forms-collapse" aria-controls="forms-collapse">Forms</button>
-                    <ul class="list-unstyled ps-3 collapse" id="forms-collapse">
-                    <li><a class="d-inline-flex align-items-center rounded" href="#overview">Overview</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#disabled-forms">Disabled forms</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#sizing">Sizing</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#input-group">Input group</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#floating-labels">Floating labels</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#validation">Validation</a></li>
-                    </ul>
-                </li>
-                <li class="my-2">
-                    <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#components-collapse" aria-controls="components-collapse">Components</button>
-                    <ul class="list-unstyled ps-3 collapse" id="components-collapse">
-                    <li><a class="d-inline-flex align-items-center rounded" href="#accordion">Accordion</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#alerts">Alerts</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#badge">Badge</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#breadcrumb">Breadcrumb</a></li>
-                    <li><a class="d-inline-flex align-items-center rounded" href="#buttons">Buttons</a></li>
-                    </ul>
-                </li>
+        <div id="collapse practice" class="bg-primary">
+            <p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#collapseExample" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                Link with href
+            </a>
+            </p>
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                </div>
+            </div>
+
+        </div>
+
+        <div>
+            <div class="form-group">
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-primary"><font-awesome-icon icon="fa-solid fa-magnifying-glass" style="color: #ffffff;" /></span>
+                        <input type="text" class="form-control text-dark"  placeholder="검색어를 입력하세요" aria-label="Recipient's username" aria-describedby="button-addon2" style="border-color: #e83283;"
+                        data-bs-toggle="collapse" data-bs-target="#collapseSearch" href="#collapseSearch" aria-expanded="false" aria-controls="collapseSearch">
+                    </div>
+                </div>
+            </div>
+            <div class="collapse" id="collapseSearch">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center text-dark">
+                        Cras justo odio
+                        <span class="badge bg-primary rounded-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center text-dark">
+                        Dapibus ac facilisis in
+                        <span class="badge bg-primary rounded-pill">2</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center text-dark">
+                        Morbi leo risus
+                        <span class="badge bg-primary rounded-pill">1</span>
+                    </li>
                 </ul>
-            </nav>
-        </aside>
+            </div>
+            <div class="container bg-primary">
+                <button class="btn btn-success collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#content" aria-expanded="false" aria-controls="collapse">
+                            Test Me
+                        </button>
+                <div class="collapse" id="content">
+                    Testy test test text that was collapsed.
+                </div>
+            </div>
+        </div>
+        <div class="row margin-top">
+            <div class="col">
+                <div class="d-grid gap-2">
+                <button type="button" class="btn btn-success" style="height: auto;" data-bs-toggle="collapse" data-bs-target="#collapse-b1" aria-expanded="true" aria-controls="collapse-b1">
+                    Button 1
+                </button>
+                </div>
+            </div>
+
+            <div class="col">
+                    <div class="d-grid gap-2">
+                    <button type="button" class="btn btn-primary" style="height: auto;" data-bs-toggle="collapse" data-bs-target="#collapse-b2" aria-expanded="false" aria-controls="collapse-b2">
+                        Button 2
+                    </button>
+                    </div>
+                </div>
+
+                </div>
+
+                <div id="myGroup">  <!-- Add the parent id here -->
+
+                <div class="collapse" id="collapse-b1"  data-bs-parent="#myGroup">
+                    <!-- <div class="alert alert-success mt-3">
+                    Text 1
+                    </div> -->
+                    <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center text-dark">
+                        Cras justo odio
+                        <span class="badge bg-primary rounded-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center text-dark">
+                        Dapibus ac facilisis in
+                        <span class="badge bg-primary rounded-pill">2</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center text-dark">
+                        Morbi leo risus
+                        <span class="badge bg-primary rounded-pill">1</span>
+                    </li>
+                </ul>
+                </div>
+
+                <div class="collapse" id="collapse-b2" data-bs-parent="#myGroup">
+                    <div class="alert alert-primary mt-3">
+                    Text 2
+                    </div>
+                </div>
+
+        </div>
     </div>
 </template>
+
+<script>
+
+export default {
+
+  methods: {
+    clickInput () {
+      alert('click input')
+    }
+  }
+}
+</script>
+<style scoped>
+    input::placeholder {
+        color: darkgrey;
+    }
+</style>
--- src/views/ExamplePopoverTooltip.vue
+++ src/views/ExamplePopoverTooltip.vue
@@ -1,102 +1,139 @@
 <template>
-  <div class="about">
-    <h1>This is an example page</h1>
+  <div style="background-color: palevioletred">
+    <div class="about">
+      <h1>This is an example page</h1>
+    </div>
+    <div class="alert alert-dismissible alert-warning">
+      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      <h4 class="alert-heading">Warning!</h4>
+      <p class="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>
+    </div>
+    <div class="container">
+      <h3 class="text-light">Popovers</h3>
+      <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="left" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Left</button>
+      <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Top</button>
+      <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Bottom</button>
+      <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Right</button>
+      <br/><br/>
+      <h3 class="text-light">Tooltips</h3>
+      <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Tooltip on left">Left</button>
+      <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Tooltip on top">Top</button>
+      <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="Tooltip on bottom">Bottom</button>
+      <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Tooltip on right">Right</button>
+      <br/><br/>
+      <!-- Button trigger modal -->
+      <h3 class="text-light">Modal</h3>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Launch static backdrop modal
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header bg-light">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body bg-light">
+              <p>I will not close if you click outside me. Don't even try to press escape key.</p>
+            </div>
+            <div class="modal-footer bg-light">
+              <button type="button" class="btn btn-dark bg-dark" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary bg-primary">Understood</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br/><br/>
+
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>&nbsp;
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Open modal for @fat</button>&nbsp;
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="recipient-name" class="col-form-label">Recipient:</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="mb-3">
+                  <label for="message-text" class="col-form-label">Message:</label>
+                  <textarea class="form-control" id="message-text"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Show a second modal and hide this one with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+      </div>
+    </div>
   </div>
-  <div class="alert alert-dismissible alert-warning">
-  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-  <h4 class="alert-heading">Warning!</h4>
-  <p class="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>
 </div>
-  <div class="container">
-    <h3 class="text-light">Popovers</h3>
-    <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="left" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Left</button>
-    <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Top</button>
-    <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Bottom</button>
-    <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-bs-original-title="Popover Title">Right</button>
-    <br/><br/>
-    <h3 class="text-light">Tooltips</h3>
-    <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Tooltip on left">Left</button>
-    <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Tooltip on top">Top</button>
-    <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="Tooltip on bottom">Bottom</button>
-    <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Tooltip on right">Right</button>
-    <br/><br/>
-    <!-- Button trigger modal -->
-    <h3 class="text-light">Modal</h3>
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-      Launch static backdrop modal
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header bg-light">
-            <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body bg-light">
-            <p>I will not close if you click outside me. Don't even try to press escape key.</p>
-          </div>
-          <div class="modal-footer bg-light">
-            <button type="button" class="btn btn-dark bg-dark" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary bg-primary">Understood</button>
-          </div>
-        </div>
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Hide this modal and show the first with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
       </div>
     </div>
-    <br/><br/>
+  </div>
+</div>
+<button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Open first modal</button>
+    </div> <!-- <div class="container"> -->
 
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>&nbsp;
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Open modal for @fat</button>&nbsp;
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+  </div>
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="mb-3">
-                <label for="recipient-name" class="col-form-label">Recipient:</label>
-                <input type="text" class="form-control" id="recipient-name">
-              </div>
-              <div class="mb-3">
-                <label for="message-text" class="col-form-label">Message:</label>
-                <textarea class="form-control" id="message-text"></textarea>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Send message</button>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-  </div> <!-- <div class="container"> -->
-
-</template>
+  </template>
 
 <script>
-import { Popover, Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+// import { Popover, Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+import { Popover, Tooltip } from 'bootstrap/dist/js/bootstrap.min.js'
 
 export default {
   mounted () {
-    // inti Popover
+  // inti Popover
     Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]'))
       .forEach(popoverNode => new Popover(popoverNode))
+    // .forEach(popoverNode => this.$data.$popover(popoverNode))
     Array.from(document.querySelectorAll('button[data-bs-toggle="tooltip"]'))
-      .forEach(tooltipNode => new Tooltip(tooltipNode))
-
+      .forEach(popoverNode => new Tooltip(popoverNode))
+    // .forEach(popoverNode => this.$data.$tooltip(popoverNode))
     const exampleModal = document.getElementById('exampleModal')
     exampleModal.addEventListener('show.bs.modal', function (event) {
-      // Button that triggered the modal
+    // Button that triggered the modal
       const button = event.relatedTarget
       // Extract info from data-bs-* attributes
       const recipient = button.getAttribute('data-bs-whatever')
