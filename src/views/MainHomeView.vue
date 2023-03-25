1fdc795b3a3d9a5090e4dd3e93b7412734e0ece8
DEP <arkduni@gmail.com>
2023-03-26 01:43:06 +0900

    브라우저 너비에 따라 사이드바 표시 여부 css 적용 작업

--- src/views/MainHomeView.vue
+++ src/views/MainHomeView.vue
@@ -1,14 +1,12 @@
 <template>
     <div></div>
     <div class="container bd-layout">
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-sm-3">
                 <MenuBar></MenuBar>
             </div>
-
             <div class="col-sm-9">
-                <!-- <MainContents></MainContents> -->
-                <!--  -->
+
                 <div class="row">
                     <div class="col-8 col-sm-8">
                         <div>
@@ -17,10 +15,25 @@
 
                     </div>
 
-                    <div class="col-4 col-sm-4" >
+                    <div  id="side_bar"  class="col-4 col-sm-4" >
                         <SideBar></SideBar>
                     </div>
                 </div>
+            </div>
+
+        </div> -->
+        <div class="row">
+            <div class="col-3">
+                <MenuBar></MenuBar>
+            </div>
+            <div id="main_contents" class="col-6">
+                <div>
+                    <router-view name="rmain"></router-view>
+                </div>
+
+            </div>
+            <div  id="side_bar"  class="col-3" >
+                <SideBar></SideBar>
             </div>
 
         </div>
@@ -37,3 +50,17 @@ export default {
 
 }
 </script>
+<style scoped>
+    @media screen and (max-width: 1320px) {
+        #side_bar {
+            display: none;
+
+            padding: 0;
+            margin: 0;
+        }
+    }
+    #main_contents {
+        min-width: 620px;
+    }
+
+</style>
