33ed0ba26da9d2f6815287cf5197b080001c8534
DEP <arkduni@gmail.com>
2023-03-28 21:08:41 +0900

    Revert "no message"
    This reverts commit 41827f0debb33edeffcfbde9dbdee9644550de69.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-
+  name: 'HomeView',
   components: {
     HelloWorld
   }
