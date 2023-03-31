2f337a1d5429184e1daca5b86599f3c3a5dc1766
kyeunghee <kyeunghj.D@gmail.com>
2023-03-31 12:46:23 +0900

    메인 피드 목록 스프링과 연결 완료  //피드 경로 재 설정해야함

--- src/components/FeedStatus.vue
+++ src/components/FeedStatus.vue
@@ -1,48 +1,49 @@
 <template>
     <div>
         <article class="my-3" id="FeedList">
-                    <div  v-for="(row , idx) in FeedList" :key="idx" class="list-group list-group-flush" id="FeedList_GF">
+                    <div  v-for="(item , idx) in items" :key="{idx}" class="list-group list-group-flush" id="FeedList_GF">
                         <!--게시글피드로 가는링크-->
-                        <a href="/게시글피드" class="list-group-item" id="FeedList_GI">
+                        <a :href="item.post_link/{post_no}" class="list-group-item" id="FeedList_GI">
 
                          <div class="row">
                             <div class="col-sm-1" style="margin-left:-20px; position: fixed;" >
                                 <!--개인 프로필로 가는 링크-->
-                                <a href="/main/mypage" ><img :src=row.user_image  width="50" height="50" class="rounded-circle" alt="user_profile" > </a>
+                                <a href="/main/mypage" ><img :src="item.profile_image"  width="50" height="50" class="rounded-circle" alt="user_profile" > </a>
                             </div>
 
                                 <!--개인 프로필로 가는 링크-->
                             <div class="col-sm-11" style="margin-left:15px">
-                            <div class="d-flex w-50 justify-content-between" id="GoUserprofile">
-                                <a href="/main/mypage"><p class="FeedList_username">{{row.nickname}}
-                                <small class="FeedList_regdate">{{row.data}}</small></p></a>
-                            </div>
+                                <div class="d-flex w-50 justify-content-between" id="GoUserprofile">
+                                    <a href="/main/mypage"><p class="FeedList_username">{{item.user_nick}}
+                                    <small class="FeedList_regdate">{{item.post_date}}</small></p></a>
+                                </div>
 
                             <div class="FeedList_contents">
-                                <p style="color: black;" class="mb-1">{{row.contents}} </p>
+                                <p style="color: black;" class="mb-1">{{item.text_content}} </p>
                             </div>
-                            <!--댓글창 , 좋아요 -->
+                            
+                            <!--댓글창 , 좋아요 , 게시글공유 , 인사이트 -->
                             <div class="FeedList_activeicont">
                                 <div class="row">
                                     <div class="col-sm-3" id="FL_spancomment">
-                                       <a class="btn" href="/댓글">
+                                       <a class="btn"  @on-click="fn_pushComment()">
                                         <font-awesome-icon icon="fa-regular fa-comment"/>
-                                        <span>{{ row.comment }}</span>
+                                        <span>{{ item.comment_cnt}}</span>
                                         </a>
                                     </div>
                                     <div class="col-sm-3"  id="FL_spanlike">
-                                        <a class="btn" href="/좋아요">
+                                        <a class="btn"  @on-click="fn_pushLike()">
                                         <font-awesome-icon  icon="fa-regular fa-heart"/>
-                                        <span>{{ row.like }}</span>
+                                        <span>{{ item.like_cnt }}</span>
                                         </a>
                                     </div>
                                     <div class="col-sm-3"  id="FL_spanshare">
-                                        <a class="btn" href="/공유">
+                                        <a class="btn" @on-click="fn_pushLink()" :href="item.post_link">
                                         <font-awesome-icon icon="fa-regular fa-share-from-square"/>
                                         </a>
                                     </div>
                                     <div class="col-sm-3"  id="FL_spanchart">
-                                        <a class="btn" href="/인사이트">
+                                        <a class="btn"  @on-click="fn_pushInsite()">
                                         <font-awesome-icon icon="fa-solid fa-chart-simple"/>
                                         </a>
                                     </div>
@@ -50,7 +51,7 @@
                             </div>
 
                             </div>
-                         </div>
+                          </div>
                         </a>
                         <hr style="color:#b0b0b0; margin:0px;">
                     </div>
@@ -61,81 +62,13 @@
 
 <script>
 export default {
-    data() {
-        return {
-            FeedList: {},
+    props: {
+        items : {type: Object , default:null }
         }
-    },
-     mounted () {
-    this.fnGetFeedList()
-  },
-  methods: {
-    fnGetFeedList() {
-      this.FeedList = [
-        {
-          nickname: 'user_jungha',
-          user_image: require('../assets/profile.jpg'),
-          data: '1시간 전',
-          contents: '어느날 머리에서 뿔이 자랐다',
-          comment: '3',
-          like: '4034'
-        },
-        {
-          nickname: 'user_jungha',
-          user_image: require('../assets/profile.jpg'),
-          data: '2시간 전',
-          contents: '이세계에서 환생한 내가 어쩌다 sss급',
-          comment: '3',
-          like: '4422'
-        },
-        {
-          nickname: '안녕하세요반가워요',
-          user_image: require('../assets/giggle1.png'),
-          data: '3시간 전',
-          contents: '이세계에서 환생한 내가 어쩌다 sss급이 된 건에 관하여,이세계에서 환생한 내가 어쩌다 sss급이 된 건에 관하여,이세계에서 환생한 내가 어쩌다 sss급이 된 건에 관하여,이세계에서 환생한 내가 어쩌다 sss급이 된 건에 관하여',
-          comment: '3',
-          like: '4'
-        },
-        {
-          nickname: 'user_4',
-          user_image: require('../assets/profile02.jpg'),
-          data: '4시간 전',
-          contents: '귀엽다',
-          comment: '3',
-          like: '42'
-        },
-        {
-          nickname: 'user_5',
-          user_image: require('../assets/logo.png'),
-          data: '3월 22일',
-          contents: '스벅',
-          comment: '3',
-          like: '14'
-        },
-        {
-          nickname: 'user_6',
-          user_image: require('../assets/profile.jpg'),
-          data: '1일 전',
-          contents: '맛있다',
-          comment: '3',
-          like: '4334'
-        },
-        {
-          nickname: 'user_7',
-          user_image: require('../assets/profile.jpg'),
-          data: '14초',
-          contents: '오늘의 아아',
-          comment: '3',
-          like: '324'
-        }
-      ]
     }
-  }
-}
 </script>
 
 <style>
-
 #FeedList {
     margin : auto;
     padding: 12px 8px;
@@ -171,6 +104,8 @@ export default {
     margin-top: 10px;
     margin-left: 6%;
 }
+
+
 
 .FeedList_activeicont #FL_spancomment,
 .FeedList_activeicont #FL_spanlike,
