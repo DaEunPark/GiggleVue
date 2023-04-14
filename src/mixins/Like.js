0964a35f2e122268e346f0e0b5a2f313de464e00
“agoutiti” <ahgnuj@gmail.com>
2023-04-14 12:24:45 +0900

    라이크 무한하트 수정중, postDetailView에서 Like관련 전부 삭제했습니다,,

--- src/mixins/Like.js
+++ src/mixins/Like.js
@@ -1,46 +1,48 @@
 export const Like = {
-  data () {
-    return {
-      // likeResult: ''
-    }
-  },
-  methods: {
-    fn_pushLike () {
-      console.log(this.ike_no)
-      // eslint-disable-next-line no-unused-vars
-      const data = { content: this.like_no }
-
-      this.$axios.post(`${this.$serverUrl}/pushLike`, {
-        user_no: this.$store.state.loginUserDTO.user_no,
-        post_no: this.post_no,
-        like_no: this.like_no
-      }).then(res => {
-        alert(res.data)
-        // this.likeResult = res.data
-        // console.log(`Like.js ${this.likeResult}`)
-        if (this.likeResult === 'Y') {
-          alert('like합니다.')
-        } else {
-          alert(' unlike 합니다.')
-        }
-      }).catch(err => {
-        console.log(err.data)
-      })
-      location.reload()
+    data () {
+      return {
+        likeResult: '',
+        // post_no:'',
+      }
     },
-    likeCount (like) {
-      this.$axios.get(`${this.$serverUrl}/likeCount`, {
-        params: {
-          user_no: like.user_no,
-          post_no: like.post_no,
-          like_no: like.like_no
-        }
-      }).then(res => {
-        this.likeResult = res.data
-        console.log(`Like.js liked: ${this.likeResult}`)
-      }).catch(err => {
-        console.log(err.data)
-      })
+    methods: {
+      fn_pushLike(post_no) {
+        console.log(post_no)
+        const data = { content: post_no }
+  
+        this.$axios.post(`${this.$serverUrl}/pushLike`, {
+          user_no: this.$store.state.loginUserDTO.user_no,
+          post_no: post_no,
+          
+          }).then(res => {
+            alert(res.data)
+          //this.likeResult = res.data
+         // console.log(`Like.js ${this.likeResult}`)
+          if (this.likeResult === 'Y') {
+            res.data ==
+             alert(`like합니다.`)
+          } else {
+             alert(` unlike 합니다.`)
+          }
+        }).catch(err => {
+          console.log(err.data)
+        })
+        location.reload()
+      },
+      likeCount (like) {
+        this.$axios.get(`${this.$serverUrl}/likeCount`, {
+          params: {
+            user_no: like.user_no,
+            post_no: like.post_no,
+            
+          }
+        }).then(res => {
+          this.likeResult = res.data
+          console.log(`Like.js liked: ${this.likeResult}`)
+        }).catch(err => {
+          console.log(err.data)
+        })
+      }
     }
   }
-}
+  
