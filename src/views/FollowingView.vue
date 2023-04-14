d58ae81afa76dc42147a722d4603a3611835b262
9ddaeng <yoo_jjj@naver.com>
2023-04-14 11:47:05 +0900

    차단페이지 / 검색 제외 / 세팅에서 친구 목록 출력되게

--- src/views/FollowingView.vue
+++ src/views/FollowingView.vue
@@ -1,71 +1,103 @@
 <template>
-    <div class="container" style="padding:0;">
-        <div class="myFriend border-round-radious">
-            <button class="btn btn_link" @click="goBack" style = "margin:0; padding:0;">
-                <h3 class ="list-group-item text-dark border-primary my-2">
-                    <font-awesome-icon class="mx-2" :icon="['fas', 'caret-left']" />My Friend
-                </h3>
-            </button>
-            <div class="tabItems">
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" data-bs-toggle="tab" href="#following" aria-selected="false" role="tab" tabindex="-1">팔로잉</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" data-bs-toggle="tab" href="#follow" aria-selected="true" role="tab">팔로우</a>
-                    </li>
-                </ul>
-                <div id="myTabContent" class="tab-content px-3">
-                    <div class="tab-pane fade active show" id="following" role="tabpanel">
-                        <p>내가 팔로우 하는 목록</p>
+  <div class="container" style="padding:0;">
+      <div class="myFriend border-round-radious">
+          <button class="btn btn_link" @click="goBack" style = "margin:0; padding:0;">
+              <h3 class ="list-group-item text-dark border-primary my-2">
+                  <font-awesome-icon class="mx-2" :icon="['fas', 'caret-left']" />My Friend
+              </h3>
+          </button>
+          <div class="tabItems">
+              <ul class="nav nav-tabs" role="tablist">
+                  <li class="nav-item" role="presentation">
+                      <a class="nav-link active" data-bs-toggle="tab" href="#following" aria-selected="false" role="tab" tabindex="-1">팔로잉</a>
+                  </li>
+                  <li class="nav-item" role="presentation">
+                      <a class="nav-link" data-bs-toggle="tab" href="#follow" aria-selected="true" role="tab">팔로우</a>
+                  </li>
+              </ul>
+              <div id="myTabContent" class="tab-content px-2">
+                  <div class="tab-pane fade active show mb-3" id="following" role="tabpanel">
+                      <!-- <myfriend :items="allFollowingList" style="color:black;"></myfriend> -->
 
+                      <div  v-for="(item , idx) in items" :key="{idx}" class="followListWrap"  @click="intoProfile(`${user.user_no}`)">
+                <div class="user_row mb-2 px-3">
+                    <div class="profileWrap">
+                        <div class="profile-img">
+                            <img :src="`${ item.profile_image }`"  width="50" height="50" class="follow_profileImage rounded-circle my-1" style="margin-right:10px;">
+                        </div>
+                        <div class="profile-nick text-dark text-nowrap fw-bold" style="overflow: hidden; width: 7.4em;">
+                            {{ item.user_nick }}
+                        </div>
+                        <div class="profile-status">
+                            {{ item.status_message }}
+                        </div>
                     </div>
-                    <div class="tab-pane fade" id="follow" role="tabpanel">
-                        <p>나를 팔로우 하는 목록</p>
-                    </div>
+                    <div v-if="following === 'N'" class="user_follow_btn" @click="followThisUser(item.user_no)"> <span class="text-nowrap + `{idx}`">팔로우</span></div>
+                    <div v-if="following === 'Y'" class="user_following_btn" @click="followThisUser(item.user_no)"> <span class="text-nowrap">팔로잉</span></div>
                 </div>
             </div>
-        </div>
-    </div>
-  </template>
+
+
+
+                  </div>
+                  <div class="tab-pane fade mb-3" id="follow" role="tabpanel">
+
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
 
 <script>
+// import myfriend from '@/components/FollowList.vue'
 
 export default {
+components:{
+  // myfriend
+},
+data() {
+  return{
+      allFollowingList: {},
+      user_no: this.$store.state.loginUserDTO.user_no
+  }
+  },
   mounted () {
-    this.getFollowingList()
+      this.getList()
   },
   methods: {
-    goBack () {
-      // eslint-disable-next-line no-unused-expressions
-      this.$router.go(-1); [2]
-    },
-    getFollowingList () {
-      this.user_no = this.$store.state.loginUserDTO.user_no
-      this.$axios.post(this.$serverUrl + '/followingList/' + this.user_no)
-        .then((res) => {
-          console.log('this.followingList = ' + this.user_no)
-        }).catch((err) => {
-          if (err.message.indexOf('Network Error') > -1) {
-            alert('오류 발생 삐리삐리')
+      getList() {
+          this.$axios.get(this.$serverUrl + '/followingList/' + `${this.user_no}`, {
+      // params: this.requestBody,
+      // headers: {},
+          params: {
+          user_no: this.user_no
           }
-        })
-    }
+          }).then((res) => {
+          this.allFollowingList = res.data
+          // 데이터 주체 확인용 console.log("this.allfeddList = "+   this.allfeedList[1].post_no)
+          }).catch((err) => {
+              if (err.message.indexOf('Network Error') > -1) {
+              alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
+              }
+          })
+      }
   }
 }
+
 </script>
 <style>
 .nav-tabs{
-    width:100%;
-    display: flex;
-    justify-content: center;
+  width:100%;
+  display: flex;
+  justify-content: center;
 }
 .nav-item{
-    width: 45%;
-    text-align: center;
+  width: 45%;
+  text-align: center;
 }
 .nav-item :active {
-    font-weight: bolder;
-    color : hotpink;
+  font-weight: bolder;
+  color : hotpink;
 }
 </style>
--- src/views/settings/SettingFriendView.vue
+++ src/views/settings/SettingFriendView.vue
@@ -8,15 +8,14 @@
                 <div class="tabItems">
                     <ul class="nav nav-tabs" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link active" data-bs-toggle="tab" href="#following" aria-selected="false" role="tab" tabindex="-1">팔로잉</a>
+                        <a class="nav-link active" data-bs-toggle="tab" href="#following" aria-selected="false" role="tab" tabindex="-1" @click="state0Change()">팔로잉</a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" data-bs-toggle="tab" href="#follow" aria-selected="true" role="tab">팔로우</a>
+                        <a class="nav-link" data-bs-toggle="tab" href="#follow" aria-selected="true" role="tab" @click="state1Change()">팔로우</a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" data-bs-toggle="tab" href="#block" aria-selected="true" role="tab">차단</a>
+                        <a class="nav-link" data-bs-toggle="tab" href="#block" aria-selected="true" role="tab" @click="state2Change()">차단</a>
                     </li>
-
                     </ul>
                     <div id="myTabContent" class="tab-content px-3">
                         <div class="tab-pane fade active show" id="following" role="tabpanel">
@@ -33,14 +32,167 @@
             </div>
         </form>
     </div>
-  </template>
+
+    <!--내가 팔로우한 사람들 목록-->
+    <div class="followingList list-group-item list-group-item-action align-items-start d-flex justify-content-between align-items-center" v-for="(user, user_nick) in followList" :key="user_nick" :items="followList" id="tempButton">
+                <div class="profile-wrapper" v-if='this.state==0 && !this.state==1 && !this.state==2' >
+                <button type="button" id="profileBtn"  @click="intoProfile(`${user.user_no}`)">
+                <div class="profile-box">
+                <img :src="`${ user.profile_image }`" id="recUserProfile" >
+                </div>
+                </button>
+                </div>
+                <div class="profile-user-name">
+                <div class="mb-1 text-dark d-inline-block text-truncate text-nowrap fw-bold">
+                    {{ user.user_nick }}</div>
+                <div id="status">{{ user.status_message }}</div>
+            </div>
+            <!-- <a href="#" @click="followThisUser(user.user_no)" class="hover-change-color"><span class="badge rounded-pill bg-success text-nowrap text-size-custom">팔로우</span></a> -->
+    </div>
+
+    <!--나를 팔로우한 사람들 목록-->
+    <div class="followuser list-group-item list-group-item-action align-items-start d-flex justify-content-between align-items-center" v-for="(followuser, user_nick) in followerList" :key="user_nick" :items="followerList" id="tempButton">
+            <div class="profile-wrapper" v-if='!this.state==0 && this.state==1 && !this.state==2' >
+                <button type="button" id="profileBtn"  @click="intoProfile(`${followuser.user_no}`)">
+                <div class="profile-box">
+                <img :src="`${ followuser.profile_image }`" id="recUserProfile" >
+                </div>
+                </button>
+                </div>
+                <div class="profile-user-name">
+                <div class="mb-1 text-dark d-inline-block text-truncate text-nowrap fw-bold">
+                    {{ followuser.user_nick }}</div>
+                <div id="status">{{ followuser.status_message }}</div>
+            </div>
+            <!-- <a href="#" @click="followThisUser(user.user_no)" class="hover-change-color"><span class="badge rounded-pill bg-success text-nowrap text-size-custom">팔로우</span></a> -->
+
+    </div>
+
+    <!--내가 차단한 사람들 목록-->
+    <div class="blockList list-group-item list-group-item-action align-items-start d-flex justify-content-between align-items-center" v-for="(blockList, user_nick) in blockList" :key="user_nick" :items="blockList" id="tempButton">
+            <div class="profile-wrapper" v-if='!this.state==0 && !this.state==1 && this.state==2' >
+                <button type="button" id="profileBtn"  @click="intoProfile(`${blockList.user_no}`)">
+                <div class="profile-box">
+                <img :src="`${ blockList.profile_image }`" id="recUserProfile" >
+                </div>
+                </button>
+                </div>
+                <div class="profile-user-name">
+                <div class="mb-1 text-dark d-inline-block text-truncate text-nowrap fw-bold">
+                    {{ blockList.user_nick }}</div>
+                <div id="status">{{ blockList.status_message }}</div>
+            </div>
+            <!-- <a href="#" @click="followThisUser(user.user_no)" class="hover-change-color"><span class="badge rounded-pill bg-success text-nowrap text-size-custom">팔로우</span></a> -->
+
+    </div>
+</template>
 
 <script>
 import MenuBar from '../../components/components-MenuBarComp-left.vue'
 import SideBar from '../../components/rightsidebars/SideBarComp.vue'
 export default {
   // eslint-disable-next-line vue/no-unused-components
-  components: { MenuBar, SideBar }
+  components: { MenuBar, SideBar },
+  data() {
+    return {
+        user_no : this.$store.state.loginUserDTO.user_no,
+        followList: {},
+        state: 0,
+        followerList: {},
+        blockList: {}
+    }
+  },
+  mounted() {
+    this.getFollowingList()
+  },
+  methods: {
+    getFollowingList() {
+        this.followerList = {}
+        this.blockList = {}
+        
+        const data= { user_no : this.user_no }
+
+        this.$axios.post(this.$serverUrl + '/followingList', JSON.stringify(data), {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((res) => {
+            console.log("res = " + res)
+            this.state = 0
+            this.followList = res.data
+          })
+    },
+    getFollowList() {
+
+    },
+    getBlockList() {
+
+    },
+    followThisUser () {
+      const data = {
+        user_no: this.$store.state.loginUserDTO.user_no,
+        follow_user: this.$store.state.loginUserDTO.follow_user
+      }
+
+      this.$axios.post(this.$serverUrl + '/follow', JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => {
+        console.log("팔로우합니다.")
+      })
+    },
+    intoProfile (user) {
+      // alert(user)
+      const data = { user_no: user }
+
+      console.log('user_nick = ' + data)
+
+      this.$axios.post(this.$serverUrl + '/otherProfile', JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => {
+        this.$store.commit('addOtherUser', res.data)
+        console.log(this.$store.state.otherUserDTO)
+        location.href = '/main/notmypage/' + this.$store.state.otherUserDTO.user_nick
+      })
+    },
+    state0Change() {
+        this.getFollowingList()
+    },
+    state1Change() {  
+        this.followList = {}
+        this.blockList = {}
+        const data= { user_no : this.user_no }
+
+        this.$axios.post(this.$serverUrl + '/followerList', JSON.stringify(data), {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((res) => {
+            console.log("res = " + res.data)
+            this.state = 1
+            this.followerList = res.data
+          })       
+    },
+    state2Change() {  
+        this.followList = {}
+        this.followerList = {}
+
+        const data= { user_no : this.user_no }
+
+        this.$axios.post(this.$serverUrl + '/blockList', JSON.stringify(data), {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((res) => {
+            console.log("res = " + res.data)
+            this.state = 2
+            this.blockList = res.data
+          })       
+    },   
+  }
 
 }
 </script>
@@ -53,5 +205,15 @@ export default {
 .nav-item{
     width: 30%;
     text-align: center;
+}
+
+.hover-change-color:hover span {
+          /* color:orangered; */
+    background-color: deeppink !important;
+}
+
+#profileBtn {
+    background-color: transparent;
+    border: none;
 }
 </style>
